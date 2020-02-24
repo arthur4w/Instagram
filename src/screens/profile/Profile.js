f81eba90971eb5ecf5772ad69fc2caeeb6461a4f
Aakash Tyagi <arthur4w@gmail.com>
2020-02-24 23:42:59 +0530

    Profile page

--- src/screens/profile/Profile.js
+++ src/screens/profile/Profile.js
@@ -5,7 +5,7 @@ import Header from '../../common/header/Header';
 class Profile extends Component {
     render() {
         return (
-            <div><Header/></div>
+            <div><Header>The Profile Page will be designed here.</Header></div>
         )
     }
 }
